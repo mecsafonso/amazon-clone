a32c768cfd0c4e27bf22fa45026e0c451a249451
Maximiliano Afonso <mecsafonso@gmail.com>
2021-02-01 20:54:05 -0300

    firebase config

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB8VfVcGFaWIxQY4wirCy9otUBALIokaFw",
+    authDomain: "clone-fd820.firebaseapp.com",
+    projectId: "clone-fd820",
+    storageBucket: "clone-fd820.appspot.com",
+    messagingSenderId: "756035501700",
+    appId: "1:756035501700:web:741f4a644c485937000cd1",
+    measurementId: "G-CTC43HKT47"
+  };
